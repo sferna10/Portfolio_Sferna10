856d01d349ec7978c045cefdf454c852b059658a
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-14 19:47:28 -0500

    Hero page coding and debugging

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,23 +1,39 @@
-import { FaPinterest, FaInstagram,FaFacebook } from "react-icons/fa";
-import HeroPic from "../assets/abstrac-background.jpg";
+import { MdFacebook } from "react-icons/md";
+import { TfiInstagram } from "react-icons/tfi";
+import { PiPinterestLogoFill } from "react-icons/pi";
+import HeroPic from "../assets/HeroPic.jpg";
 
 const hero = () => {
   return (
     <section className='flex justify-around items-center p-10 space-x-10 lg:flex-row ssm:flex-col ssm:space-y-10 text-white'>
       <div className='lg:w-1/3 ssm:w-fit'>
-         <p className='text-4x1 mb-5 text-slate-300'>I'm</p>
+         <p className='text-4x1 mb-5 text-slate-300'>I am</p>
          <h1 className='text-6x1'>Silvia Fernandez</h1>
          <hr/>
-            <p className='nt-10 text-6x1-xl text-slate-300 font-sans'>
-            A full stalk developer In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, </p>
+            <p className='mt-10 text-slate-300 font-sans'>
+            A full stalk developer, and web graphic designer.</p>
       </div>
-      <div className="w-1/3 items-center ssm:w-fit">
-      <img src={HeroPic} alt="woman" width={{250} height={250} className="rounded-full w-full border-8  border-white"}/div>
-      <div>
-      <p>About Me</p>
-      <p>
+      <div className='w-1/3 items-center ssm:w-fit'>
+      <img src={HeroPic} alt="woman" width={250} height={250} 
+      className='rounded-full w-full border-8  border-white'/>
+      </div>
+
+      <div className="w-1/3 ssm:w-fit">
+      <p className='text-4x1 mb-4'>About Me</p>
+      <p className=' text-slate-300'>
+      Web designer is my passion.
       
       </p>
+      <button className='bg-white text-indigo-600 px-10 py-2 my-3 rounded-full hover:bg-indigo-800 hover:text-white '>
+         Show more...
+      </button>
+
+      <div className='flex mt-5 space-x-4 cursor-pointer'>
+          
+          <TfiInstagram size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
+          <MdFacebook size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
+          <PiPinterestLogoFill size={40} className='border-4 hover:border-indigo-800 rounded-full'></PiPinterestLogoFill>
+      </div>
       </div>
     </section>
   )
