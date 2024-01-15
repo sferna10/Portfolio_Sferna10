7f5de462f94d41af5ab10cf8ab42f3f658833a68
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-14 20:40:57 -0500

    Navbar debugging

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -3,9 +3,11 @@
 const nav = () => {
   return (
     <>
-      <div>
-        <div>Professional Portfolio</div>
+      <div className="flex items-center justify-between p-10 lg:flex-row">
         <div>
+        <a href="#" className="text-white font-mono text-3xl tracking-wider flex items-center">Professional Portfolio</a>
+        </div>
+        <div className="space-x-4">
           <a href="#">Home</a>
           <a href="#">Skills</a>
           <a href="#">Projects</a>
