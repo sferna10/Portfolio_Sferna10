bad15aeda88e2cc0487f6f0880c3b24149cacc16
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-16 19:53:13 -0500

    Woorking 0n project file

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -7,21 +7,23 @@ import Project4Img from "./Assets/Project4.png";
 import Project5Img from "./Assets/Project5.png";
 import Project6Img from "./Assets/Project6.png";
 export default function Projects() {
-  return (
-        <>
+ 
+    return (
+        <div>
             <div className="max-w-full m-20 bg-indigo-800">
                 <div className="grid m justify-items-center">
                     <h1 className="text-3xl text-white mt-10">Projects</h1>
                 </div>
 
                 <div>
-                  <div className="grid grid-cols-2 gap-4 p-5">
-                       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
-                        </div>
+                
+                    <div className="grid grid-cols-2 gap-4 p-5">
+                       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse"></div>
                        <hr/>
-                   <div className="grid grid-cols-2 gap-4 p-5">      
+                    
+                        <div className="grid grid-cols-2 gap-4 p-5"></div>
+                   </div>
                 </div>
             </div>
-        </>
+        </div>
     );
-}
