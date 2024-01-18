0bc379e1b9da6d7a476b7e302d3ee304bdb626bc
Silvia A. Fernandez <139423719+sferna10@users.noreply.github.com>
2024-01-17 22:16:30 -0500

    New review

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -89,7 +89,7 @@ export default function Projects() {
                                 </p>
                                </div>
                             </div>
-                            </div>
+                        </div>
                     <div>
                </div>
             </div>
